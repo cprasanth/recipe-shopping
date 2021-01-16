a6e6dee70cbe382d29bac52f413cc957239dfc05
cprasanth <prasanthcpanicker@gmail.com>
2021-01-16 18:53:12 +0000

    Updated alert message

--- components/RecipeListItem.tsx
+++ components/RecipeListItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, TouchableHighlight } from 'react-native';
+import { StyleSheet, TouchableHighlight, Alert } from 'react-native';
 import { View, Text } from '../components/Themed';
 import { useAppDispatch } from '../AppStateProvider';
 import Colors from '../constants/Colors';
@@ -7,20 +7,20 @@ import Colors from '../constants/Colors';
 const RecipeListItem = ({ name, id, onPress }: any) => {
   const dispatch = useAppDispatch();
 
+  const addToShoppingList = (id: string) => {
+    dispatch({
+      type: 'ADD_TO_LIST',
+      payload: { id: id },
+    });
+    Alert.alert('Added to shopping list', `Ingredients for ${name} added to your shopping list`);
+  };
   return (
     <TouchableHighlight underlayColor={Colors.light.tint} onPress={onPress}>
       <View style={styles.recipeInfo}>
         <View style={styles.details}>
           <Text style={styles.title}>{name}</Text>
         </View>
-        <TouchableHighlight
-          onPress={() =>
-            dispatch({
-              type: 'ADD_TO_LIST',
-              payload: { id: id },
-            })
-          }
-        >
+        <TouchableHighlight onPress={() => addToShoppingList(id)}>
           <View style={styles.buttonIcon}>
             <Text style={styles.buttonText}>Add to basket</Text>
           </View>
